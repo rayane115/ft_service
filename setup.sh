86f63f38688fa6ddc4d6ee83d303c4249dfd3057
user42 <rayane.qouchich@gmail.com>
2020-12-21 19:52:40 +0100

    deuxieme push

--- setup.sh
+++ setup.sh
@@ -4,6 +4,35 @@
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-#cp srcs/metallb/metallb-conf.yaml srcs/metallb/config.yaml
-#sed -ie "s/MINIKUBE_IP/$IP/g" srcs/metallb/config.yaml
+
 kubectl apply -f srcs/metallb/metallb-conf.yaml
+
+
+######################################
+#          Docker build              #
+######################################
+eval $(minikube docker-env)
+echo "\e[93mBuilding Nginx:\e[0m"
+docker build -t my_nginx srcs/nginx/
+eval $(minikube docker-env -u)
+
+######################################
+#          Config YAML               #
+######################################
+kubectl apply -f nginx-pod.yaml
+
+######################################
+#        Dashboard kubernetes        #
+######################################
+
+xterm -hold -e "minikube dashboard" &
+
+echo -ne 	"\n\n\033[1;36m
+███████╗████████╗     ███████╗███████╗██████╗ ██╗   ██╗██╗ ██████╗███████╗███████╗
+██╔════╝╚══██╔══╝     ██╔════╝██╔════╝██╔══██╗██║   ██║██║██╔════╝██╔════╝██╔════╝
+█████╗     ██║        ███████╗█████╗  ██████╔╝██║   ██║██║██║     █████╗  ███████╗
+██╔══╝     ██║        ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██║██║     ██╔══╝  ╚════██║
+██║        ██║███████╗███████║███████╗██║  ██║ ╚████╔╝ ██║╚██████╗███████╗███████║
+╚═╝        ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝ ╚═════╝╚══════╝╚══════╝
+e)
+                                                                                  \n\n"
